30fd833d1e1df411a6a422fe916e925769a98c82
Alexander 'Aux' Ivanov <aux@hexmode.org>
2013-04-29 10:37:40 +0300

    Added JavaDoc, removed onFail()

--- library/src/com/grilledmonkey/lepropepyaka/Pepyaka.java
+++ library/src/com/grilledmonkey/lepropepyaka/Pepyaka.java
@@ -4,26 +4,65 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Messenger;
 
+/**
+ * Всё общение приложения с LeproActivia происходит с помощью Pepyaka.
+ * Нам надо просто дёрнуть статический метод askStatus() и передать ему
+ * listener. Когда сервис соизволит ответить, listener об этом сразу узнает.
+ * 
+ * Также тут мы видим несколько констант, менять их не надо (: Константы
+ * STATUS_* расскажут нам кто есть текущий юзер. Все виды не леперов
+ * меньше единицы.
+ * 
+ * @author Aux
+ *
+ */
 public class Pepyaka {
 	public static final String REQUEST_INTENT = "com.grilledmonkey.leproactivia.ATATA";
-	public static final String RESPONSE_INTENT = "com.grilledmonkey.leproactivia.PISH";
 	public static final String IS_LOGGED_IN = "isLoggedIn";
 	public static final String CALLBACK_MESSENGER = "callbackMessenger";
 
 	public static final int CALLBACK_BITCH = 0;
 
+	/**
+	 * Если у вас lepra-only приложение, то получив данный статус можно
+	 * предложить юзеру поставить LeproActivia и радоваться.
+	 */
+	public static final int STATUS_FAIL = -1;
+	/**
+	 * 146% не лепер! Бей его, ребзя!
+	 */
+	public static final int STATUS_NOT_LEPER = 0;
+	/**
+	 * Братюня!
+	 */
+	public static final int STATUS_LEPER = 1;
+
+	/**
+	 * Этот метод инициирует связь с сервисом LeproActivia и просит его
+	 * вернуть статус юзера. Listener получит статус во время
+	 * обработки onResult().
+	 * 
+	 * @param listener
+	 */
 	public static void askStatus(OnResultListener listener) {
 		Intent intent = new Intent(Pepyaka.REQUEST_INTENT);
 		Messenger messenger = new Messenger(new PepyakaHandler(listener));
 		intent.putExtra(Pepyaka.CALLBACK_MESSENGER, messenger);
 		if(listener.getContext().startService(intent) == null) {
-			listener.onFail();
+			listener.onResult(STATUS_FAIL);
 		}
 	}
 
+	/**
+	 * Слушатель статуса юзера. Нам также требуется Context для успешного
+	 * общения с сервисом, поэтому слушатель должен вернуть текущий Context
+	 * в методе getContext(). А без этого будет Null Pointer Exception.
+	 * 
+	 * @author Aux
+	 *
+	 */
 	public static interface OnResultListener {
-		public void onResult(boolean isLoggedIn);
-		public void onFail();
+		public void onResult(int status);
 		public Context getContext();
 	}
 }
--- library/src/com/grilledmonkey/lepropepyaka/PepyakaHandler.java
+++ library/src/com/grilledmonkey/lepropepyaka/PepyakaHandler.java
@@ -5,26 +5,37 @@ import java.lang.ref.WeakReference;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 
+/**
+ * Хендлер для Messenger. Мы обращаемся к сервису LeproActivia и передаём ему
+ * ссылку на этот хендлер. После выполнения своей работы, сервис LeproActivia
+ * пошлёт нам в ответ информация и сработает handleMessage().
+ * 
+ * В конструктор необходимо передать Pepyaka.OnResultListener. Тащемта это всё
+ * делается автоматически при общении с классов Pepyaka, сюда лазить кагбе
+ * и не надо.
+ * 
+ * @author Aux
+ *
+ */
 public class PepyakaHandler extends Handler {
-	private static final String LOG_TAG = "PepyakaHandler";
-
 	private final WeakReference<Pepyaka.OnResultListener> listener;
 
 	public PepyakaHandler(Pepyaka.OnResultListener listener) {
 		this.listener = new WeakReference<Pepyaka.OnResultListener>(listener);
 	}
 
+	/**
+	 * Сервис нам чё-та заслал, проверяем данные и если всё сходится - отправляем
+	 * результат на родину. Если сервис пиздит, то и хуй с ним.
+	 */
 	@Override
-    public void handleMessage(Message message) {
+	public void handleMessage(Message message) {
 		Bundle data = message.getData();
 		Pepyaka.OnResultListener listener = this.listener.get();
 
-		Log.w(LOG_TAG, "message.what: " + message.what);
-
 		if(listener != null && message.what == Pepyaka.CALLBACK_BITCH && data != null) {
-			listener.onResult(data.getBoolean(Pepyaka.IS_LOGGED_IN, false));
+			listener.onResult(data.getBoolean(Pepyaka.IS_LOGGED_IN, false) ? Pepyaka.STATUS_LEPER : Pepyaka.STATUS_NOT_LEPER);
 		}
 	}
 }
