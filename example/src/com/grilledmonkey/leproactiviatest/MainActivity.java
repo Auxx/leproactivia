4436db63ed87d6c8bde163dac3f08961d10af486
Alexander 'Aux' Ivanov <aux@hexmode.org>
2013-04-29 10:46:27 +0300

    Added JavaDoc, removed onFail()

--- example/src/com/grilledmonkey/leproactiviatest/MainActivity.java
+++ example/src/com/grilledmonkey/leproactiviatest/MainActivity.java
@@ -1,14 +1,25 @@
 package com.grilledmonkey.leproactiviatest;
 
-import com.grilledmonkey.lepropepyaka.Pepyaka;
-import com.grilledmonkey.lepropepyaka.Pepyaka.OnResultListener;
-
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-import android.app.Activity;
-import android.content.Context;
 
+import com.grilledmonkey.lepropepyaka.Pepyaka;
+import com.grilledmonkey.lepropepyaka.Pepyaka.OnResultListener;
+
+/**
+ * Примитивный пример использования библиотеки LeproPepyaka для определения
+ * типа пользователя.
+ * 
+ * В данном примере статус юзера определяется по тапу на кнопку, обычно же
+ * его определять имеет смысл при старте приложения. Тут стоит помнить,
+ * что общение с сервисом LeproActivia происходит асинхронно.
+ * 
+ * @author Aux
+ *
+ */
 public class MainActivity extends Activity implements OnResultListener {
 
 	@Override
@@ -17,23 +28,42 @@ public class MainActivity extends Activity implements OnResultListener {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public void onResult(boolean isLoggedIn) {
-		Toast.makeText(this, "Status: " + (isLoggedIn ? "LEPER!" : "HUJ ):"), Toast.LENGTH_LONG).show();
+	/**
+	 * По клику на кнопку мы просим пепяку пообщацо с активией.
+	 * 
+	 * @param v
+	 */
+	public void handleClick(View v) {
+		Pepyaka.askStatus(this);
 	}
 
+	/**
+	 * Для работы с сервисами пепяке необходимо знать контекст приложения.
+	 * Кстати, Activity - это Context (:
+	 */
 	@Override
 	public Context getContext() {
 		return(this);
 	}
 
+	/**
+	 * Сервис нам что-то ответил, ща разберёмся!
+	 */
 	@Override
-	public void onFail() {
-		Toast.makeText(this, "FAIL WHALE!", Toast.LENGTH_LONG).show();
-	}
-
-	public void handleClick(View v) {
-		Pepyaka.askStatus(this);
+	public void onResult(int status) {
+		String msg;
+		switch(status) {
+			case Pepyaka.STATUS_NOT_LEPER:
+				msg = "Не лепер!";
+				break;
+			case Pepyaka.STATUS_LEPER:
+				msg = "Братюня!";
+				break;
+			default:
+				msg = "LeproActivia не установлена.";
+				break;
+		}
+		Toast.makeText(this, "Status: " + msg, Toast.LENGTH_LONG).show();
 	}
 
 }
